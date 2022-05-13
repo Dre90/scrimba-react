1f8514d0872b5c2cafc12563f7583de1ad276711
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2022-05-13 12:32:58 +0200

    done with React facts

--- react-facts/src/components/Navbar.js
+++ react-facts/src/components/Navbar.js
@@ -1,3 +1,14 @@
+import "../Navbar.css";
+import logo from "../images/react-icon-small.png"
+
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="logo" className="logo-image"></img>
+        <h3 className="logo-text">ReactFacts</h3>
+      </div>
+      <h4>React Course - Project 1</h4>
+    </nav>
+    );
 }
